91e3057a86cabadd90c5503a3b2a88a66ac839ab
Alex Hlatsko <alexei.hlatsko@gmail.com>
2022-03-02 17:53:41 +0100

    removed unnecessary comments
    in main.js removed comments

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,4 @@
-// const menuToggle = document.querySelector('.menuToggle');
 const menuToggle = document.getElementById('menuToggle');
-// const navigation = document.querySelector('.navigation');
 const navigation = document.getElementById('navigation');
 menuToggle.onclick  =  function(){
     menuToggle.classList.toggle('active')
